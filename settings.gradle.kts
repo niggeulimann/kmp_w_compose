2db379d340e4e6bad693b0b1b59d9a8534f82145
Uli Niggemann <uli.niggemann@getslash.de>
2023-05-04 11:23:54 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMP"
+include(":androidApp")
+include(":shared")
+
+
